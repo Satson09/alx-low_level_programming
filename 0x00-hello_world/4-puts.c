7eac9dad7e33ffc589ffc54b55f17d4757096b96
satson09 <satsunjohnson@gmail.com>
2023-02-18 13:28:22 +0100

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
  /*
   * File: 4-puts.c
-  * Auth: satson johnson <satsunjohnson@gmail.com>
   */
 
   #include <stdio.h>
 
  /*
-  * main- Prints "programming is like is like building a multilingual
+  * main - Entry point
+  *
+  * Description: "programming is like is like building a multilingual
   *               puzzle, follow by new line
   *
   * Return: Always 0.
