f7deb3ef10b7467a2a23adb3574da7fc03e7a2c6
satson09 <satsunjohnson@gmail.com>
2023-02-18 11:15:11 +0100

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,20 @@
 /**
- * program that print exact statement 
- * including haeder file
+ * main: print "programming is like is like building a multilingual
+ *              puzzle, follow by new line
+ *
+ * including header file
+ *
  * including input
- * return function
+ *
+ * return 0.
  */
 
-include <stdio.h>
-int Main(void)
+#include <stdio.h>
+
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
+	
 	return (0);
 }
 		
