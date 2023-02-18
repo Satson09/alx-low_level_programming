34b410bc5b1b73f391d0e9f4a8c007f72ec82357
satson09 <satsunjohnson@gmail.com>
2023-02-18 10:55:47 +0100

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+/**
+ * program that print exact statement 
+ * including haeder file
+ * including input
+ * return function
+ */
+
+include <stdio.h>
+int Main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
+		
