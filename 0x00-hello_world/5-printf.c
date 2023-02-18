5ff4c700ad0d0f58a39ad1372d87f7a21fa28911
satson09 <satsunjohnson@gmail.com>
2023-02-18 14:17:49 +0100

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * File: 5-printf.c
+ *
+ * Description: with proper grammer, but the outcome is a pieces of art
+ *
+ * main - input function
+ *
+ * Return: Always 0
+ */
+	int main(void)
+{
+	printf("with proper grammer, but the outcome is a pieces of art\n");
+	
+	return (0);
+}
