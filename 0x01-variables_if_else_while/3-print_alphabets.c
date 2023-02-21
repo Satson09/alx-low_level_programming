874ef424622035e69adb720caa8182a1ec3f2a9c
satson09 <satsunjohnson@gmail.com>
2023-02-21 20:01:59 +0100

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,10 +10,10 @@ int main(void)
 	char b = 'a';
 	char d = 'A';
 
-	for (b = 'a'; b <= 'z'; b++);
+	while (b = 'a'; b <= 'z'; b++;)
 		putchar(b);
 
-	for (d = 'A'; d <= 'Z'; d++);
+	while (d = 'A'; d <= 'Z'; d++;)
 		putchar(d);
 
 	putchar('\n');
