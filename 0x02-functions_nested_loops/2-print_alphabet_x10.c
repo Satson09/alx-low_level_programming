1558154e06b5334c01dc0c9efcd330e79ec38b0b
satson09 <satsunjohnson@gmail.com>
2023-02-26 15:56:19 +0100

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - prints alphabet 10 times, in lowercase, followed by a new line
+ *
+ * assigning values and variable to data type
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+	int i;
+		for (i = 0; i < 10; i++)
+		{
+			for (ch = 'a'; ch < 'z'; ch++)
+			{
+				_putchar(ch);
+			}
+			_putchar('\n');
+		}
+		return (0);
+}
+
+
+
