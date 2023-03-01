0af84d68aa81d44656856cf1db6c106248c3a119
satson09 <satsunjohnson@gmail.com>
2023-03-01 19:11:21 +0100

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -13,13 +13,13 @@ int main(void)
 		int pass[100];
 		int i, sum, n;
 
-		sum = 0;	
+		sum = 0;
 
 		srand(time(NULL));
 
 		for (i = 0; i < 100; i++)
 		{
-			pass[i] - rand() % 78;
+			pass[i] = rand() % 78;
 			sum += (pass[i] + '0');
 			putchar(pass[i] + '0');
 			if ((2772 - sum) - '0' < 78)
@@ -32,4 +32,3 @@ int main(void)
 		}
 		return (0);
 }
-				
