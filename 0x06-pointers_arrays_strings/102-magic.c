55237332a6c6be2901b467597b1d4571581d8843
satson09 <satsunjohnson@gmail.com>
2023-03-03 17:02:22 +0100

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - Add one line to code, to print program a[2] = 98
- *
- * initializing variables to data type
- *
- * Return: always 0 (success)
- *
- */
 int main(void)
 {
 	int n;
@@ -15,10 +7,17 @@ int main(void)
 	int *p;
 
 	a[2] = 1024;
-	*p = &n;
-
+	p = &n;
+/**
+* write your line of code here...
+* Remember:
+* you are not allowed to use a
+* you are not allowed to modify p
+* only one statement
+* you are not allowed to code anything else than this line of code
+*/
 	*(p + 5) = 98;
-
+ /* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
-	 return (0);
+	return (0);
 }
