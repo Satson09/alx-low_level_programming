e26c7d08410e28aa098617b2846965b4abae00a1
satson09 <satsunjohnson@gmail.com>
2023-03-03 14:02:29 +0100

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-/* main - Add one line to code, to print program a[2] = 98
- * 
+/**
+ * main - Add one line to code, to print program a[2] = 98
+ *
  * initializing variables to data type
  *
  * Return: always 0 (success)
- * 
+ *
  */
 int main(void)
 {
@@ -13,11 +14,11 @@ int main(void)
 	int a[5];
 	int *p;
 
-	a[2] = 1024
-	*p = &n
+	a[2] = 1024;
+	*p = &n;
 
 	*(p + 5) = 98;
 
 	printf("a[2] = %d\n", a[2]);
-	    return (0);
+	 return (0);
 }
