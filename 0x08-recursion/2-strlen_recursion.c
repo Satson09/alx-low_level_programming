6de998b09ffa4d4f0c746bb7220cd2961f066f8d
satson09 <satsunjohnson@gmail.com>
2023-03-09 23:23:16 +0100

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - that returns the length of a string.
+ *
+ * @s: the pointer variable to string 
+ * Return: the lenght of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+	{
+		return (0);
+	}
+}
