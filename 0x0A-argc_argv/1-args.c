3b85a9a7977cfece9804122c449fd340ebaf31c7
satson09 <satsunjohnson@gmail.com>
2023-03-15 20:04:57 +0100

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,22 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-	int count;
-
-	printf("This program was called with \"%s\"\n", argv[0]);
-
-	if (argc > 1)
-	{
-		for (count = 1; count < argc; count++)
-		{
-			printf("argv[%d] = %s\n", count, argv[count]);
-		}
-	}
-	else
-	{
-		printf("The command had no other arguments.\n");
-	}
-
+	(void)argv;
+	printf("%d\n", (argc - 1));
 	return (0);
 }
-
