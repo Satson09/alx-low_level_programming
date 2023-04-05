7c15c1f46fa795b70ae30a3ce21e4cbc0467c54b
satson09 <satsunjohnson@gmail.com>
2023-04-05 15:18:17 +0100

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,28 +1,44 @@
 #include "main.h"
 
 /**
-* _strstr - locate a substring.
-* @haystack: char array
-* @needle: char array (keyword)
-*
-* Return: array
-*/
+ *strstr - function that locates a substring.
+ *@haystack: first value -char
+ *@needle: second value - char
+ *
+ * Return: char with result
+ */
 char *_strstr(char *haystack, char *needle)
 {
-	while (*haystack != '\0')
-	{
-		char *duplicate = haystack;
+	int count = 0, a = 0, k, j = 0, l, i = 0;
+	char *p;
 
-		while (*haystack == *needle && *haystack != '\0' && *needle != '\0')
-		{
-			needle++;
-			haystack++;
-		}
-		if (!*needle != '\0')
-		{
-			return (duplicate);
-		}
-		haystack++;
+	while (needle[count] != '\0')
+	{
+		count++;
 	}
-	return (0);
+	while (haystack[i] != '\0')
+	{
+		i++;
+			}
+	if (count == 0)
+		return (haystack);
+	while (haystack[j] != '\0')
+	{
+		if (haystack[j] == needle[0])
+		{
+			p = &haystack[j];
+			l = j;
+			a = 0;
+			for (k = 0; k < count; k++)
+			{
+				if (haystack[l] == needle[k])
+					a++;
+				l++;
+			}
+		}
+		if (a == count)
+			return (p);
+		j++;
+	}
+	return ('\0');
 }
